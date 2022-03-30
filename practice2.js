fd4f7ff2cc41d0ed2ef3ca6da264dc5485a1f160
nospame <mapson.evan@gmail.com>
2022-03-30 16:45:59 -0700

    add problem 10 solution and work on bonus problems

--- practice2.js
+++ practice2.js
@@ -130,7 +130,7 @@ console.log(
 
 // 9) Write a function that takes in an array of numbers and returns true if all the numbers are bigger than 10, otherwise returns false.
 function allBiggerThanTen(numbers) {
-  var allBigger = true;
+  // var allBigger = true;
   // numbers.forEach(function (number) {
   //   if (number <= 10) {
   //     allBigger = false;
@@ -151,10 +151,131 @@ console.log(
 
 
 // 10) Write a function that takes in an array of words and returns the number of times the letter “a” appeared in total.
+function numberOfAs(words) {
+  let count = 0;
+  words.forEach(function (word) {
+    word.split('').forEach(function (char) {
+      if (char === 'a') {
+        count++;
+      }
+    });
+  });
+  return count;
+}
+
+console.log(
+  numberOfAs(["apple", "aardvark", "banana"])
+);
+
+
+
 // (solutions to half of the ruby problems: https://gist.github.com/peterxjang/a9a3827be5077abe008b62f6f9900d71)
 
 
 // BONUS PROBLEMS
 // Write a function that accepts a string and returns whether it’s a palindrome.
+function isPalindrome(string) {
+  let reversedString = string.split('').reverse().join('');
+  if (reversedString === string) {
+    return true;
+  }
+  return false;
+}
+
+console.log(
+  isPalindrome("tacocat")
+);
+
 // Write a function to generate/print/store the first “n” prime numbers.
+function primeNumbers(n) {
+  let primes = [];
+  for (let i = 2; primes.length < n; i++) {
+    let isPrime = true;
+    for (let j = 0; j < primes.length; j++) {
+      if (i % primes[j] === 0) {
+        isPrime = false;
+        break;
+      }
+    }
+    if (isPrime) {
+      primes.push(i);
+    }
+  }
+  return primes;
+}
+
+console.log(
+  primeNumbers(12)
+);
+
 // Given a tic-tac-toe board (matrix of 3 x 3), write a function that can check to see whether X or O won.
+function whoWon(matrix) {
+  var winner = '';
+  let i = 0;
+  while (i < 3) {
+    let j = 0;
+    let across = [];
+    let down = [];
+    let diagFwd = [matrix[0][0], matrix[1][1], matrix[2][2]];
+    let diagBwd = [matrix[0][2], matrix[1][1], matrix[2][0]];
+    while (j < 3) {
+      across[j] = matrix[i][j];
+      down[j] = matrix[j][i];
+      j++;
+    }
+    if (
+      across.join('') === "XXX" ||
+      down.join('') === "XXX" ||
+      diagFwd.join('') === "XXX" ||
+      diagBwd.join('') === "XXX"
+    ) {
+      winner = 'X';
+    } else if (
+      across.join('') === "OOO" ||
+      down.join('') === "OOO" ||
+      diagFwd.join('') === "OOO" ||
+      diagBwd.join('') === "OOO"
+    ) {
+      winner = 'O';
+    }
+    i++;
+  }
+  return `Winner: ${winner}`;
+}
+
+console.log(
+  whoWon([
+    ['X', 'O', 'X'],
+    ['O', 'X', 'X'],
+    ['O', 'X', 'X']
+  ])
+);
+
+// possible wins: 
+// down
+// 00, 10, 20 => j i, j i, j i (1st outer loop)
+// 01, 11, 21 => j i, j i, j i (2nd outer loop)
+// 02, 12, 22 => j i, j i, j i (3rd outer loop)
+
+// across
+// 00, 01, 02 => i j, i j, i j (1st outer loop)
+// 10, 11, 12 => i j, i j, i j (2nd outer loop)
+// 20, 21, 22 => i j, i j, i j (3rd outer loop)
+
+// diagonal fwd
+// 00, 11, 22 => i i, i i, i i
+
+// diagonal bwd
+// 02, 11, 20 => hard code?
+
+// loop sequence
+// J: 0 I: 0
+// J: 1 I: 0
+// J: 2 I: 0
+// J: 0 I: 1
+// J: 1 I: 1
+// J: 2 I: 1
+// J: 0 I: 2
+// J: 1 I: 2
+// J: 2 I: 2
+
