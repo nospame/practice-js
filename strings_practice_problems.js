fbe26579b7d82180978b283d43058ae4eba84114
nospame <mapson.evan@gmail.com>
2022-03-30 17:19:44 -0700

    add js strings practice solutions

---
+++ strings_practice_problems.js
@@ -0,0 +1,35 @@
+// # 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
+var firstName = "Jim";
+var lastName = "Brundberg";
+console.log(firstName + ' ' + lastName);
+
+// # 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+console.log(`${firstName} ${lastName}`);
+
+
+// # 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
+var color1 = "purple";
+var color2 = "orange";
+var color3 = "turquoise";
+console.log("The big " + color1 + " elephant turned positively " + color2 + " when he saw his friend dressed in " + color3 + "!");
+
+// # 5. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
+console.log(`The large ${color1} elephant turned absolutely ${color2} after seeing his friend fully clothed in ${color3}.`);
+
+
+// # 7. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string concatenation (the + operator).
+var bookTitle = "The Left Hand of Darkness";
+var author = "Ursula K. Le Guin";
+console.log(bookTitle + " was written by " + author + ".");
+
+// # 8. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string interpolation (the #{} operator).
+console.log(`${author} wrote the book ${bookTitle}.`)
+
+
+// # 10. Write a program that uses variables to store the names of three cities, then prints out a sentence using that information with string concatenation (the + operator).
+var city1 = "Paris";
+var city2 = "Cambridge";
+var city3 = "Seattle";
+console.log("Something about the cities of " + city1 + ", " + city2 + ", and " + city3 + ".");
+
+// # SOLUTIONS: https://gist.github.com/peterxjang/79ba5acc912a86cce2a18c2f14b1c712
