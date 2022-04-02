4f312fb7836a088cfca5ba3ec05475554d62a884
nospame <mapson.evan@gmail.com>
2022-04-02 11:16:25 -0700

    add loops practice js and html file solutions

---
+++ loops2_practice_problems.js
@@ -0,0 +1,82 @@
+// # 1. Write a while loop to print the numbers 1 through 10.
+var i = 1;
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
+
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+}
+
+// # 2. Write a while loop that prints the word "hello" 5 times.
+i = 0;
+while (i < 5) {
+  console.log("hello");
+  i++;
+}
+for (let i = 0; i < 5; i++) {
+  console.log("hello");
+}
+
+
+// # 4. Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
+i = 0;
+while (i <= 100) {
+  console.log(i);
+  i += 5;
+}
+
+for (let i = 0; i <= 100; i += 5) {
+  console.log(i);
+}
+
+// # 5. Write a while loop that prints the number 9000 ten times.
+i = 0;
+while (i < 10) {
+  console.log(9000);
+  i++;
+}
+
+for (let i = 0; i < 10; i++) {
+  console.log(9000);
+}
+
+
+// # 7. Write a while loop that prints the numbers 50 to 70.
+i = 50;
+while (i <= 70) {
+  console.log(i);
+  i++;
+}
+
+for (let i = 50; i <= 70; i++) {
+  console.log(i);
+}
+
+
+// # 8. Write a while loop that prints the phrase "Around the world" 144 times.
+i = 0;
+while (i < 144) {
+  console.log("Around the world");
+  i++;
+}
+
+for (let i = 0; i < 144; i++) {
+  console.log("Around the world");
+}
+
+
+// # 10. Write a while loop that prints the even numbers from 2 to 40.
+i = 2;
+while (i <= 40) {
+  console.log(i);
+  i += 2;
+}
+
+for (let i = 0; i <= 40; i += 2) {
+  console.log(i);
+}
+
+
+// # SOLUTIONS: https://gist.github.com/peterxjang/c4ec0e0f8f6e123d65ada9bf3100068b
